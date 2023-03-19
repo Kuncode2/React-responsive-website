47776474dce7f7d70ac3a776dce6ed6f498beafc
kunal <kuncode2@gmail.com>
2023-03-19 18:27:04 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+
+
+function App(){
+    return (
+        <>
+        <nav className='navbar'>
+            
+        </nav>
+        </>
+    )
+}
+export default Navbar
